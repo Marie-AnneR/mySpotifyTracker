2a8eb2fae144b5897b1ad5e7755d2bef27368b41
Marie-Anne <marie-anne.roth@epitech.eu>
2026-09-23 16:56:58 +0200

    feat(auth): implement Spotify login with PKCE
    Refs #5

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import SpotifyLoginButton from '@/components/SpotifyLoginButton';
+
 
 export default function Home() {
   return (
@@ -37,6 +39,8 @@ export default function Home() {
             </a>{" "}
             center.
           </p>
+          <SpotifyLoginButton />
+
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
